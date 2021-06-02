ffb89c372000b6dae4c78c76bebe4284a7bb93ec
Franek Stepek <franek.stepek@gmail.com>
2021-06-02 17:16:26 +0200

    Validation of repo existence, added function for RSFragment Dialogs

--- app/src/main/java/pl/edu/pwr/student/actions_feed/ui/repository_selection/RepositorySelectionFragment.kt
+++ app/src/main/java/pl/edu/pwr/student/actions_feed/ui/repository_selection/RepositorySelectionFragment.kt
@@ -10,11 +10,12 @@ import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import pl.edu.pwr.student.actions_feed.dao.Repository
 import pl.edu.pwr.student.actions_feed.dao.RepositoryDatabase
 import pl.edu.pwr.student.actions_feed.databinding.FragmentRepositorySelectionBinding
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class RepositorySelectionFragment : Fragment() {
     private lateinit var repositoryListAdapter: RepositorySelectionListAdapter
@@ -53,18 +54,34 @@ class RepositorySelectionFragment : Fragment() {
     private fun addRepository(repoPath: String) {
         //First check if is already in list
         if(!repositoryList.contains(repoPath)) {
-            repositoryList.add(repoPath)
-            repositoryListAdapter.notifyItemInserted(repositoryList.size - 1)
-            lifecycleScope.launch(Dispatchers.IO) {
-                database.repositoryDao().addRepository(Repository(0, repoPath))
+            checkExistence(repoPath)
+        } else {
+            showDialogWindow("Duplication", "You are already watching this repo")
+        }
+    }
+
+    private fun checkExistence(repoPath : String) = runBlocking {
+        var flag = true
+        val job = CoroutineScope(Dispatchers.IO).launch {
+            val url = URL("https://github.com/$repoPath")
+            val connection = url.openConnection() as HttpsURLConnection
+            try {
+                if(connection.responseCode == HttpsURLConnection.HTTP_NOT_FOUND)
+                    flag = false
+            } finally {
+                connection.disconnect()
             }
         }
-        else {
-            val builder = AlertDialog.Builder(this.context)
-            builder.setTitle("Duplication")
-            builder.setMessage("You are already watching this repo")
-            builder.setPositiveButton("OK") { _: DialogInterface, _: Int -> }
-            builder.show()
+        job.join()
+        if (flag) acceptRepository(repoPath)
+        else showDialogWindow("Lack of Existence", "That repo does not exist")
+    }
+
+    private fun acceptRepository(repoPath: String) {
+        repositoryList.add(repoPath)
+        repositoryListAdapter.notifyItemInserted(repositoryList.size - 1)
+        lifecycleScope.launch(Dispatchers.IO) {
+            database.repositoryDao().addRepository(Repository(0, repoPath))
         }
     }
 
@@ -77,11 +94,15 @@ class RepositorySelectionFragment : Fragment() {
             repositoryListAdapter.deleteItem(repoPath)
         }
         else {
-            val builder = AlertDialog.Builder(this.context)
-            builder.setTitle("Lack of repo")
-            builder.setMessage("You are not watching that repo yet")
-            builder.setPositiveButton("OK") { _: DialogInterface, _: Int -> }
-            builder.show()
+            showDialogWindow("Lack of repo", "You are not watching that repo yet")
         }
+    }
+
+    private fun showDialogWindow(title: String, message: String) {
+        val builder = AlertDialog.Builder(this.context)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.setPositiveButton("OK") { _: DialogInterface, _: Int -> }
+        builder.show()
     }
 }
