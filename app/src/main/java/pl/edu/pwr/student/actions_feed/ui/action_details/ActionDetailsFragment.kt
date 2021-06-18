d5d2dae0f3eb10558eb65120108c529d2e58c107
Blazej Michalik <code.mrmino@gmail.com>
2021-06-18 07:13:41 +0200

    Remove redundant nullability

--- app/src/main/java/pl/edu/pwr/student/actions_feed/ui/action_details/ActionDetailsFragment.kt
+++ app/src/main/java/pl/edu/pwr/student/actions_feed/ui/action_details/ActionDetailsFragment.kt
@@ -23,7 +23,7 @@ class ActionDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentActionDetailsBinding.inflate(inflater, container, false)
         return binding.root
